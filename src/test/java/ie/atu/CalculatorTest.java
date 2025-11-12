4d44c3439f19340c5b768dfef5ee89a086b6c1cf
DanielMaku-STUDENT <G00467390@atu.ie>
2025-11-12 17:22:07 +0000

    Near start

---
+++ src/test/java/ie/atu/CalculatorTest.java
@@ -0,0 +1,21 @@
+package ie.atu;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    Calculator calc;
+
+    @BeforeEach
+    public void setUp()
+    {
+        calc = new Calculator();
+    }
+
+    @Test
+    public void testAdd_Success()
+    {
+        assertEquals();
+    }
+
+}
